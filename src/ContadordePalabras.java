52360e6ce649c6c1f50aeacc7de9b9bcd6d9a6a9
abigail23181 <abigailrodrigxz@gmail.com>
2026-05-27 19:17:38 -0600

    Nuria envio primer ejercicio: ContadordePalabras

--- src/ContadordePalabras.java
+++ src/ContadordePalabras.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-
-public class ContadorPalabras {
+public class ContadordePalabras {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingresa una frase: ");
